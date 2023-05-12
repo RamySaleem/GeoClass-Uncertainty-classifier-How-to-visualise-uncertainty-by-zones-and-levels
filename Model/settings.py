340c62d0f66ae4bca18dfe30fe8369fc7c5478d3
Ramy Abdallah <43496650+RamySaleem@users.noreply.github.com>
2023-05-12 01:33:14 +0100

    Add files via upload

---
+++ Model/settings.py
@@ -0,0 +1,25 @@
+query = "hor1-6.csv"
+y_dist = 100
+uncertainty_distance = 50
+targets = {
+    "tw1.csv":uncertainty_distance,
+    "tw2.csv":uncertainty_distance,
+    "tw3.csv":uncertainty_distance,
+    "tw4.csv":uncertainty_distance,
+    "tw5.csv":uncertainty_distance,
+    "tw6.csv":uncertainty_distance,
+    "tw7.csv":uncertainty_distance,
+    "tw8.csv":uncertainty_distance,
+    "tw9.csv":uncertainty_distance,
+    "tw10.csv":uncertainty_distance,
+    "tw11.csv":uncertainty_distance,
+    "tw12.csv":uncertainty_distance,
+    "tw13.csv":uncertainty_distance,
+    "tw14.csv":uncertainty_distance,
+    "oc1.csv":uncertainty_distance,
+    "oc2.csv":uncertainty_distance,
+    "oc3.csv":uncertainty_distance,
+    
+    
+
+}
